c7e8648ac2498978e6ca1b7779a509080b00f575
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2023-02-25 16:40:30 +0330

    Add accounts app to project and make custom user
    Start app accounts to project and add it in settings and make my CustomUser model and its forms and add auth user model in settings

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     # My Apps
-    
+    'accounts.apps.AccountsConfig',
     
     # Third Party Apps
     'rest_framework',
@@ -130,3 +130,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Custom User
+AUTH_USER_MODEL = 'accounts.CustomUser'
