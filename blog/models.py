74a88a01433dc943d3e21b2116fc75e4494b2e89
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2023-02-25 17:00:46 +0330

    Make post model
    Make post model in blog app and migrate it

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 
-# Create your models here.
+class post(models.Model):
+    title = models.CharField(_('Title'), max_length=200)
+    
+    slug = models.SlugField(_('Slug'))
+    
+    text = models.TextField(_('Text'))
+    
+    author = models.ForeignKey(get_user_model(), verbose_name=_('Author'), on_delete=models.CASCADE)
+    
+    status = models.BooleanField(default=False)
+    
+    
+    datetime_created = models.DateTimeField(_('datetime_created'), default=timezone.now())
+    datetime_modified = models.DateTimeField(auto_now=True, verbose_name=_('datetime_modified'))
+    
+    def __str__(self):
+        return self.title
