d2c91aca5a39f5a39f9410827fe92bee83ae1119
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2023-03-04 11:27:18 +0330

    Add post detail view in blog
    Add post detail view in blog and add it in urls and make its template

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.shortcuts import reverse
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
@@ -20,3 +21,6 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.pk])
--- blog/views.py
+++ blog/views.py
@@ -10,3 +10,13 @@ class PostListView(generic.ListView):
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
     
+
+class PostDetailView(generic.DetailView):
+    # def get_object(self):
+    #     return get_object_or_404(
+    #         Post.objects.filter(status=True),
+    #         pk = self.kwargs.get("pk")
+    #     )
+    model = Post
+    template_name = 'blog/post_detail.html'
+    
